137e04691718f65a852bf4b611101b13fa65c3a7
idaloof <99144519+idaloof@users.noreply.github.com>
2023-05-09 22:43:43 +0200

    Added use keyword for Config and Finder

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,15 +2,18 @@
 
 declare(strict_types=1);
 
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
 /**
  * Execute the command like this:
  *  php-cs-fixer --config=.php-cs-fixer.dist.php fix src tests
  */
 require_once __DIR__.'/tools/php-cs-fixer/vendor/autoload.php';
 
-$finder = PhpCsFixer\Finder::create();
+$finder = Finder::create();
 
-$config = new PhpCsFixer\Config();
+$config = new Config();
 $config->setFinder($finder);
 
 return $config;
