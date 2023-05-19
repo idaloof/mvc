f1ebe2d83be0da260e29a2ada14cd95f729ccef4
idaloof <99144519+idaloof@users.noreply.github.com>
2023-05-19 18:03:14 +0200

    Tests for quote controller done

---
+++ tests/Controller/ApiQuoteControllerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Test class for ApiQuoteController
+ */
+
+namespace App\Controller;
+
+use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
+
+class ApiQuoteControllerTest extends TestCase
+{
+    /**
+     * Verifies that response status for QuoteController is ok
+     */
+    public function testApiControllerStatusCode() : void
+    {
+        $client = new Client([
+            'base_uri' => 'https://www.student.bth.se/~mafl22/dbwebb-kurser/mvc/me/report/public/',
+            'http_errors' => false
+        ]);
+
+        $response = $client->get('api/quote');
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    /**
+     * Verifies that response from api returns correct date
+     */
+    public function testJsonQuoteDate(): void
+    {
+        $controller = new ApiQuoteController();
+        $response = $controller->jsonQuote();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $content = $response->getContent();
+        $data = is_string($content) ? json_decode($content, true) : null;
+
+        $this->assertEquals(date("Y-m-d"), $data["datum"]);
+    }
+
+    /**
+     * Verifies that response from api returns a quote
+     */
+    public function testJsonQuote(): void
+    {
+        $controller = new ApiQuoteController();
+        $response = $controller->jsonQuote();
+
+        $quote = [
+            "Muddy water is best cleared by leaving it alone.",
+            "We cannot be more sensitive to pleasure without being more sensitive to pain.",
+            "Problems that remain persistently insoluble should always be suspected as questions asked in the wrong way.",
+            "Try to imagine what it will be like to go to sleep and never wake up... now try to imagine what it was like to wake up having never gone to sleep.",
+            "You are an aperture through which the universe is looking at and exploring itself."
+        ];
+
+        $content = $response->getContent();
+        $data = is_string($content) ? json_decode($content, true) : null;
+
+        $this->assertContains($data["quote"], $quote);
+    }
+}
