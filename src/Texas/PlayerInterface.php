3302d5b550f0eaa3d02a0d66c9f434bc0fdd5c4c
idaloof <99144519+idaloof@users.noreply.github.com>
2023-05-24 21:03:51 +0200

    Created interface for player classes

---
+++ src/Texas/PlayerInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Interface Player
+ * This interface provides player methods for its implementing classes.
+ */
+
+namespace App\Texas;
+
+interface PlayerInterface
+{
+    public function getName(): string;
+
+    public function getBuyIn(): int;
+
+    public function increaseBuyIn(int $money): void;
+
+    public function decreaseBuyIn(int $money): void;
+
+    public function getHand(): TexasHand;
+
+    public function getBet(): Bet;
+
+    public function setHasFolded(): void;
+
+    public function hasFolded(): bool;
+}
