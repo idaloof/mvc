447ff6ab6bb4b329b7bf65bb8db0c3ac04834fb2
idaloof <99144519+idaloof@users.noreply.github.com>
2023-05-27 13:16:47 +0200

    Test suite for Table class complete

---
+++ tests/Texas/TableTest.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Test suite for Table class
+ */
+
+namespace App\Texas;
+use PHPUnit\Framework\TestCase;
+
+class TableTest extends TestCase
+{
+    /**
+     * @var Table $table Table object for testing.
+     */
+    private Table $table;
+
+    /**
+     * Set up before every test case.
+     */
+    protected function setUp(): void
+    {
+        $this->table = new Table();
+    }
+
+    /**
+     * Verify that Table object correctly adds and gets community cards.
+     */
+    public function testAddAndGetCards(): void
+    {
+        $card = new Card("Ace of Hearts", "AH", "H", "A");
+
+        $this->table->addToCommunityCards($card);
+
+        $exp = "App\Texas\Card";
+
+        $res = $this->table->getCommunityCards()[0];
+
+        $this->assertInstanceOf($exp, $res);
+    }
+
+    /**
+     * Verify that Table object correctly clears community cards.
+     */
+    public function testClearCommunityCards(): void
+    {
+        $card = new Card("Ace of Hearts", "AH", "H", "A");
+
+        $this->table->addToCommunityCards($card);
+
+        $this->table->clearCommunityCards();
+
+        $exp = 0;
+
+        $res = count($this->table->getCommunityCards());
+
+        $this->assertEquals($exp, $res);
+    }
+
+    /**
+     * Verifies that Table object correctly adds money to pot and gets correct amount.
+     */
+    public function testAddAndGetPot(): void
+    {
+        $this->table->addMoneyToPot(10);
+
+        $exp = 10;
+
+        $res = $this->table->getPot();
+
+        $this->assertEquals($exp, $res);
+    }
+
+    /**
+     * Verifies that Table object correctly adds money to pot and gets correct amount.
+     */
+    public function testClearAndGetPot(): void
+    {
+        $this->table->addMoneyToPot(10);
+
+        $this->table->clearPot();
+
+        $exp = 0;
+
+        $res = $this->table->getPot();
+
+        $this->assertEquals($exp, $res);
+    }
+}
