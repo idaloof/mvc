3304dc0bbb7550866c29d709ffc9a805e51f7fe6
idaloof <99144519+idaloof@users.noreply.github.com>
2023-05-27 12:40:57 +0200

    Added methods from GameLogic

--- src/Texas/Queue.php
+++ src/Texas/Queue.php
@@ -17,6 +17,21 @@ class Queue
     private array $players;
 
     /**
+     * @var PlayerInterface $smallBlindPlayer Player that has small blind at the moment.
+     */
+    private PlayerInterface $smallBlindPlayer;
+
+    /**
+     * @var PlayerInterface $bigBlindPlayer Player that has big blind at the moment.
+     */
+    private PlayerInterface $bigBlindPlayer;
+
+    /**
+     * @var PlayerInterface $dealerPlayer Player that has the dealer button at the moment.
+     */
+    private PlayerInterface $dealerPlayer;
+
+    /**
      * Class constructor
      *
      * @param array<PlayerInterface> $players Players of the game.
@@ -25,6 +40,24 @@ class Queue
     public function __construct(array $players)
     {
         $this->players = $players;
+    }
+
+    /**
+     * Sets the player roles at game start, with three players dealer starts after blinds are posted.
+     *
+     * @return array<PlayerInterface> Players in queue.
+     */
+    public function setRolesBeforeGameStart(): array
+    {
+        $this->dealerPlayer = $this->dequeue();
+        $this->smallBlindPlayer = $this->dequeue();
+        $this->bigBlindPlayer = $this->dequeue();
+
+        $this->enqueue($this->dealerPlayer);
+        $this->enqueue($this->smallBlindPlayer);
+        $this->enqueue($this->bigBlindPlayer);
+
+        return $this->players;
     }
 
     /**
@@ -69,6 +102,58 @@ class Queue
      */
     public function getQueue(): array
     {
+        return $this->players;
+    }
+
+    /**
+     * Determines which player is dealer
+     * and which players have the blinds before next round starts.
+     *
+     * @return array<PlayerInterface> array of players.
+     */
+
+    public function setQueueBeforeRoundStart(): array
+    {
+        $players = $this->getQueue();
+        $nrOfPlayers = count($players);
+
+        for ($i = 0; $i < $nrOfPlayers; $i++) {
+            $this->dequeue();
+        }
+
+        $theDealer = $this->dealerPlayer;
+
+        $this->dealerPlayer = $this->smallBlindPlayer;
+        $this->smallBlindPlayer = $this->bigBlindPlayer;
+        $this->bigBlindPlayer = $theDealer;
+
+        $this->enqueue($this->dealerPlayer);
+        $this->enqueue($this->smallBlindPlayer);
+        $this->enqueue($this->bigBlindPlayer);
+
+
+        return $this->players;
+    }
+
+    /**
+     * Shifts the players back in correct queue position
+     * so that correct player is first to act (smallBlindPlayer).
+     *
+     * @return array<PlayerInterface> array of players.
+     */
+    public function shiftPlayersBeforeCommunityCards(): array
+    {
+        $players = $this->getQueue();
+        $nrOfPlayers = count($players);
+
+        for ($i = 0; $i < $nrOfPlayers; $i++) {
+            $this->dequeue();
+        }
+
+        $this->enqueue($this->smallBlindPlayer);
+        $this->enqueue($this->bigBlindPlayer);
+        $this->enqueue($this->dealerPlayer);
+
         return $this->players;
     }
 }
