a05804fb76ff61a3a8fbb8f0fddb5438e401537f
idaloof <99144519+idaloof@users.noreply.github.com>
2023-05-28 10:24:01 +0200

    Divided method into smaller ones to decrease complexity

--- src/Texas/ComputerLogic.php
+++ src/Texas/ComputerLogic.php
@@ -6,6 +6,7 @@
  */
 
 namespace App\Texas;
+
 use App\Repository\PreFlopRankingsRepository;
 
 class ComputerLogic
@@ -14,19 +15,19 @@ class ComputerLogic
      * PreFlopRankingsRepository class which holds methods for requiring data
      * from pre_flop_rankings table of database.
      *
-     * @var PreFlopRankingsRepository $preFlopRankingsRepository 
+     * @var PreFlopRankingsRepository $flopRepo
      */
-    private PreFlopRankingsRepository $preFlopRankingsRepository;
+    private PreFlopRankingsRepository $flopRepo;
 
     /**
      * Class constructor
      *
-     * @param PreFlopRankingsRepository $preFlopRankingsRepository
+     * @param PreFlopRankingsRepository $repo
      *
      */
     public function __construct(PreFlopRankingsRepository $repo)
     {
-        $this->preFlopRankingsRepository = $repo;
+        $this->flopRepo = $repo;
     }
 
     /**
@@ -48,13 +49,13 @@ class ComputerLogic
     }
 
     /**
-     * Gets the ranking of the player's two starting cards.
+     * Gets starting card ranks as concatenated string.
      *
      * @param array<Card> $cards The two starting cards of the computer player.
      *
-     * @return string Ranking of the starting cards.
+     * @return string Starting cards as concatenated string.
      */
-    public function getPreFlopRanking(array $cards): string
+    public function getHoleRanks(array $cards): string
     {
         $ranks = "";
 
@@ -62,8 +63,53 @@ class ComputerLogic
             $ranks .= $card->getCardRank();
         }
 
-        $result = $this->preFlopRankingsRepository->findCardRanking($ranks);
-
-        return $result["rank"];
+        return $ranks;
     }
+
+    /**
+     * Gets starting cards' type as string.
+     *
+     * @param array<Card> $cards The two starting cards of the computer player.
+     *
+     * @return string Starting cards' type as string.
+     */
+    public function getHoleType(array $cards): string
+    {
+        $type = "o";
+
+        if ($cards[0]->getCardSuit() === $cards[1]->getCardSuit()) {
+            $type = "s";
+        } elseif ($cards[0]->getCardRank() === $cards[1]->getCardRank()) {
+            $type = "p";
+        }
+
+        return $type;
+    }
+
+    /**
+     * Gets starting cards' ranking as int.
+     *
+     * @param array<Card> $cards The two starting cards of the computer player.
+     *
+     * @return int Starting cards' ranking as int.
+     */
+    public function getHoleRanking(array $cards): int
+    {
+        $type = $this->getHoleType($cards);
+        $cardRanks = $this->getHoleRanks($cards);
+
+        $cardCombo = $this->flopRepo->findCardRanking($cardRanks, $type)[0];
+
+        $cardRank = intval($cardCombo->getRank());
+
+        return $cardRank;
+    }
+
+    // Cleve måste ha en riskAversion-property som ökar
+    // eller sjunker. Riskaversionen ska vara beroende av
+    // humanplayer moves, potten i förhållande till blindsen,
+    // sin egen insats i rundan i förhållande till potten,
+    // vilka kort den har på hand, hur stor chans den har att
+    // få stege/färg/två par/triss i nästa stage (flop, turn, river).???
+    //
 }
