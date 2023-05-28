f4cd9be8d22a50d6671576b63b0bedfdcfe16fc9
idaloof <99144519+idaloof@users.noreply.github.com>
2023-05-28 10:25:01 +0200

    New migration after dropping table

---
+++ migrations/Version20230528073812.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ * @SuppressWarnings(PHPMD)
+ */
+final class Version20230528073812 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE pre_flop_rankings (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, cards VARCHAR(255) NOT NULL, type VARCHAR(1) NOT NULL, rank VARCHAR(3) NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE pre_flop_rankings');
+    }
+}
