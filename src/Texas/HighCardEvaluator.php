34daed52f0a0c2ad44071be5ac8984df42872ac4
idaloof <99144519+idaloof@users.noreply.github.com>
2023-05-28 21:42:43 +0200

    Added evaluate hand method to be able to implement interface

--- src/Texas/HighCardEvaluator.php
+++ src/Texas/HighCardEvaluator.php
@@ -6,8 +6,23 @@
 
 namespace App\Texas;
 
-class HighCardEvaluator extends CalculatePoints
+class HighCardEvaluator extends CalculatePoints implements EvaluatorInterface
 {
+    /**
+     * Returns high card if hand has it or empty string if not.
+     *
+     * @param array<string> $suits      Suits of the player's cards.
+     * @param array<string> $values     Values of the player's cards.
+     * @param array<string> $ranks      Ranks of the player's cards.
+     *
+     * @return string                   "High Card".
+     *
+     */
+    public function evaluateHand(array $suits, array $values, array $ranks): string
+    {
+        return "High Card";
+    }
+
     /**
      * Calculates and returns the points for a hand.
      *
