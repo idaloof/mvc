2d3e458fdde52b6419ebcd797d6afbdbfd935ca1
idaloof <99144519+idaloof@users.noreply.github.com>
2023-05-28 10:27:16 +0200

    Tests for ComputerCleve complete

---
+++ tests/Texas/ComputerCleveTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Test suite for ComputerCleve class.
+ */
+
+namespace App\Texas;
+use PHPUnit\Framework\TestCase;
+
+class ComputerCleveTest extends TestCase
+{
+    /**
+     * @var ComputerCleve $player Player to run tests for.
+     */
+
+    private ComputerCleve $player;
+
+    /**
+     * Set up before each test
+     */
+    protected function setUp() : void
+    {
+        $this->player = new ComputerCleve("Cleve", 20);
+    }
+
+    /**
+     * Verifies that correct object is instantiated.
+     */
+    protected function testCreateObject() : void
+    {
+        $player = new ComputerCleve("Cleve", 20);
+
+        $this->assertInstanceOf("App\Texas\ComputerStu", $player);
+    }
+
+    /**
+     * Verify that ComputerCleve increases and returns correct risk level.
+     */
+    public function testIncreaseAndGetRisk() : void
+    {
+        $this->player->increaseRiskLevel(20);
+
+        $exp = 20;
+
+        $res = $this->player->getRiskLevel();
+
+        $this->assertEquals($exp, $res);
+    }
+
+    /**
+     * Verify that ComputerCleve decreases and returns correct risk level.
+     */
+    public function testDecreaseAndGetRisk() : void
+    {
+        $this->player->increaseRiskLevel(20);
+        $this->player->decreaseRiskLevel(20);
+
+        $exp = 0;
+
+        $res = $this->player->getRiskLevel();
+
+        $this->assertEquals($exp, $res);
+    }
+}
