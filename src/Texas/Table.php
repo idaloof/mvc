d846c908dac233b8dad4e51e318f16fe2c28e4ab
idaloof <99144519+idaloof@users.noreply.github.com>
2023-05-29 15:05:09 +0200

    Added blind properties and methods

--- src/Texas/Table.php
+++ src/Texas/Table.php
@@ -21,6 +21,28 @@ class Table
     private int $pot = 0;
 
     /**
+     * @var int $smallBlind The game's small blind.
+     */
+    private int $smallBlind;
+
+    /**
+     * @var int $bigBlind The game's big blind.
+     */
+    private int $bigBlind;
+
+    /**
+     * Class constructor.
+     *
+     * @param int $buyIn
+     *
+     */
+    public function __construct(int $buyIn)
+    {
+        $this->smallBlind = 0.01 * $buyIn;
+        $this->bigBlind = 0.02 * $buyIn;
+    }
+
+    /**
      * Adds card to table's community cards.
      *
      * @param Card $card Card to be added
@@ -82,5 +104,25 @@ class Table
     public function clearPot(): void
     {
         $this->pot = 0;
+    }
+
+    /**
+     * Gets small blind.
+     *
+     * @return int Small blind.
+     */
+    public function getSmallBlind(): int
+    {
+        return $this->smallBlind;
+    }
+
+    /**
+     * Gets big blind.
+     *
+     * @return int Big blind.
+     */
+    public function getBigBlind(): int
+    {
+        return $this->bigBlind;
     }
 }
