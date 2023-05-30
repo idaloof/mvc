f824aa16fd0681f0cd96aac7ba00014c3836f2c4
idaloof <99144519+idaloof@users.noreply.github.com>
2023-05-31 01:16:46 +0200

    Got rid of test js file

--- assets/app.js
+++ assets/app.js
@@ -11,6 +11,5 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
-require('./test.js');
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
