7170d5f854569d11a9b10879eeb9030713b615e3
idaloof <99144519+idaloof@users.noreply.github.com>
2023-05-30 14:24:21 +0200

    Created controller for project pages, proj API gets an own controller

---
+++ src/Controller/ProjectPagesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProjectPagesController extends AbstractController
+{
+    /* Proj Landing Route */
+    #[Route("/proj", name: "proj")]
+    public function proj(
+    ): Response {
+        return $this->render('proj/proj.html.twig');
+    }
+
+    /* Proj About Route */
+    #[Route("/proj/about", name: "proj_about")]
+    public function projAbout(
+    ): Response {
+        return $this->render('proj/about.html.twig');
+    }
+
+    /* Proj Database Route */
+    #[Route("/proj/about/database", name: "proj_database")]
+    public function projDatabase(
+    ): Response {
+        return $this->render('proj/database.html.twig');
+    }
+}
