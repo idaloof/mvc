5fd5132ef8f7b89eac9c4518f63f06d6b5bef03b
idaloof <99144519+idaloof@users.noreply.github.com>
2023-06-01 11:57:52 +0200

    Created controller for initializing game

---
+++ src/Controller/ProjectGameInitController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Messages;
+use App\Repository\MessagesRepository;
+use App\Texas\TexasGame;
+use Doctrine\DBAL\Connection;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProjectGameInitController extends AbstractController
+{
+    /* Proj PreFlop Route */
+    #[Route("/proj/game-init", name: "proj_game_init")]
+    public function projCreateGame(
+        SessionInterface $session,
+        MessagesRepository $repo,
+        ManagerRegistry $doctrine,
+        Connection $connection
+    ): Response {
+
+        /**
+         * @var TexasGame $game
+         */
+        $game = $session->get('game');
+
+        $game->setQueueAndRoles();
+        $game->takeBlindsAndAddToPot();
+        $game->dealStartingCards();
+
+        $session->set('game', $game);
+
+        $sql = [
+            'DROP TABLE IF EXISTS messages;',
+            'CREATE TABLE messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                created VARCHAR(5) NOT NULL,
+                messenger VARCHAR(255) NOT NULL,
+                "message" VARCHAR(255) NOT NULL
+            );'
+        ];
+
+        foreach ($sql as $query) {
+            $statement = $connection->prepare($query);
+            $statement->executeStatement();
+        }
+
+
+        $entityManager = $doctrine->getManager();
+
+        $message = new Messages();
+
+        date_default_timezone_set('Europe/Stockholm');
+
+        $current_time = date('H:i');
+
+        $message->setCreated(strval($current_time));
+        $message->setMessenger("Game");
+        $message->setMessage(
+            "Welcome! Play fair and good luck!"
+        );
+
+        $entityManager->persist($message);
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('proj_pre_flop');
+    }
+}
