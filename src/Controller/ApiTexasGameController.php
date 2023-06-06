46516cbee55ec961da1e502560c4a758e405e8f7
idaloof <99144519+idaloof@users.noreply.github.com>
2023-06-06 22:43:27 +0200

    Api controller mainly for testing purposes

---
+++ src/Controller/ApiTexasGameController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Texas\TexasGame;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiTexasGameController extends AbstractController
+{
+    #[Route("/proj/api-texas", name: 'proj_api_texas')]
+    public function jsonDeckShuffle(
+        SessionInterface $session
+    ): JsonResponse {
+
+        $data = [
+            'game notice' => "No game started."
+        ];
+
+        if ($session->has('game')) {
+            /**
+            * @var TexasGame $game
+            */
+            $game = $session->get('game');
+
+            $players = $game->getQueuePlayers();
+
+            $data = [];
+
+            foreach ($players as $player) {
+                $data[] = $player->getPlayerData();
+                $data[] = $player->getPlayerMoves()->getNumberOfRoundMoves();
+                $data[] = $player->getPlayerMoves()->getRoundMoves();
+            }
+        }
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+}
