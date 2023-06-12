4f15d7f919fbfcf03caf82ba32aab09b4f70e1c4
idaloof <99144519+idaloof@users.noreply.github.com>
2023-06-12 20:14:56 +0200

    New controller and templates for winner page

---
+++ src/Controller/ProjectWinnerController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MessagesRepository;
+use App\Texas\TexasGame;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProjectWinnerController extends AbstractController
+{
+    /* Proj Winner Route */
+    #[Route("/proj/winner", name: "proj_winner")]
+    public function projWinner(
+        SessionInterface $session,
+        MessagesRepository $repository
+    ): Response {
+        // $session->set('forward-route', 'proj_winner_init');
+        // $session->set('back-route', 'proj_river');
+
+        /**
+         * @var TexasGame $game
+         */
+        $game = $session->get('game');
+
+        $queuePlayers = $game->getQueuePlayers();
+
+        $queuePlayersData = [];
+
+        foreach ($queuePlayers as $player) {
+            $queuePlayersData[] = $player->getPlayerData();
+        }
+
+        $pot = $game->getPot();
+
+        $winner = $game->getWinner();
+
+        $winnerName = $winner->getName();
+        $winnerHandName = $winner->getHand()->getBestHandName();
+        $winnerCards = $winner->getHand()->getBestHandAsImages();
+
+        $winning = "hand";
+
+        if ($game->getByFold()) {
+            $winning = "fold";
+        }
+
+        $communityImages = $session->get('communityImages');
+        $session->set('game', $game);
+
+        return $this->render('proj/proj-winner.html.twig', [
+            'queuePlayers' => $queuePlayersData,
+            'restartUrl' => $this->generateUrl('proj_reset_round'),
+            'community' => $communityImages,
+            'pot' => $pot,
+            'winner' => $winnerName,
+            'winnerHand' => $winnerHandName,
+            'winnerCards' => $winnerCards,
+            'winning' => $winning
+        ]);
+    }
+}
