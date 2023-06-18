71e1af04932d575181961b0a415c6fddb792253a
idaloof <99144519+idaloof@users.noreply.github.com>
2023-06-18 11:18:25 +0200

    New route for tie

--- src/Controller/ProjectWinnerController.php
+++ src/Controller/ProjectWinnerController.php
@@ -36,12 +36,6 @@ class ProjectWinnerController extends AbstractController
         $winnerHandName = $winner->getHand()->getBestHandName();
         $winnerCards = $winner->getHand()->getBestHandAsImages();
 
-        $winning = "hand";
-
-        if ($game->isWinnerByFold()) {
-            $winning = "fold";
-        }
-
         $communityImages = $session->get('communityImages');
         $session->set('game', $game);
 
@@ -52,8 +46,47 @@ class ProjectWinnerController extends AbstractController
             'pot' => $pot,
             'winner' => $winnerName,
             'winnerHand' => $winnerHandName,
-            'winnerCards' => $winnerCards,
-            'winning' => $winning
+            'winnerCards' => $winnerCards
+        ]);
+    }
+
+    /* Proj Winner Tie Route */
+    #[Route("/proj/winner-tie", name: "proj_winner_tie")]
+    public function projWinnerTie(
+        SessionInterface $session
+    ): Response {
+        /**
+         * @var TexasGame $game
+         */
+        $game = $session->get('game');
+
+        $queuePlayers = $game->getQueuePlayers();
+
+        $queuePlayersData = [];
+
+        foreach ($queuePlayers as $player) {
+            $queuePlayersData[] = $player->getPlayerData();
+        }
+
+        $pot = $game->getPot();
+
+        $winners = $game->getWinnersTieGame();
+
+        $winnerNames = "";
+
+        foreach ($winners as $player) {
+            $winnerNames .= $player->getName() . ", ";
+        }
+
+        $communityImages = $session->get('communityImages');
+        $session->set('game', $game);
+
+        return $this->render('proj/proj-winner-tie.html.twig', [
+            'queuePlayers' => $queuePlayersData,
+            'restartUrl' => $this->generateUrl('proj_reset_round_tie'),
+            'community' => $communityImages,
+            'pot' => $pot,
+            'winner' => $winnerNames
         ]);
     }
 }
