63895cea48427be889a06e9c5f3c7b41e960d20c
idaloof <99144519+idaloof@users.noreply.github.com>
2023-06-20 09:14:34 +0200

    New class to decrease controller complexity

---
+++ src/Texas/MoneyHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * MoneyHandler trait
+ * This class is responsible for adding money to com players buyins
+ */
+
+namespace App\Texas;
+
+use Doctrine\Persistence\ManagerRegistry;
+
+trait MoneyHandler
+{
+    /**
+     * Adds money to com players' buy in if low on money.
+     *
+     * @param array<PlayerInterface> $players Array of players.
+     * @param int $buyIn Initial buy in.
+     * @param ManagerRegistry $doctrine
+     *
+     * @return string
+     */
+    public function fillComPlayersBuyIn(
+        array $players,
+        int $buyIn,
+        ManagerRegistry $doctrine
+    ): string {
+        foreach ($players as $player) {
+            $playerName = $player->getName();
+            if ($playerName === "Stu" || $playerName === "Cleve") {
+                if ($player->getBuyIn() < 0.5 * $buyIn) {
+                    $moneyToAdd = $buyIn - $player->getBuyIn();
+                    $player->increaseBuyIn($moneyToAdd);
+
+                    $message = $playerName . " utökar sitt saldo med $" . $moneyToAdd;
+
+                    $this->addMessage("Texas", $message, $doctrine);
+                }
+            }
+        }
+
+        return "ok";
+    }
+}
